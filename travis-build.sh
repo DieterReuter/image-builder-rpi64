7c8ebe72dd65d41e3ecb096d6f52d9b1c2e75e0f
Dieter Reuter <dieter.reuter@me.com>
2017-01-17 19:26:29 +0100

    Use external Travis build script

---
+++ travis-build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+set -x
+
+# This script is meant to run on Travis-CI only
+if [ -z "$TRAVIS_BRANCH" ]; then 
+  echo "ABORTING: this script runs on Travis-CI only"
+  exit
+fi
+
+# create a build number
+export BUILD_NR="$(date '+%Y%m%d-%H%M%S')"
+echo "BUILD_NR=$BUILD_NR"
+
+# run build
+#-create build dest
+BUILD_DEST=builds/$BUILD_NR
+mkdir -p $BUILD_DEST
+#-build
+make shellcheck
+VERSION=${TRAVIS_TAG} make sd-image
+#-move artifacts to build dest
+mv hypriotos-rpi64* $BUILD_DEST/
+
+
+# deploy to GitHub releases
+export GIT_TAG=v$BUILD_NR
+export GIT_RELTEXT="Auto-released by [Travis-CI build #$TRAVIS_BUILD_NUMBER/#$TRAVIS_JOB_NUMBER](https://travis-ci.com/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID)"
+curl -sSL https://github.com/tcnksm/ghr/releases/download/v0.5.4/ghr_v0.5.4_linux_amd64.zip > ghr.zip
+unzip ghr.zip
+./ghr --version
+./ghr --debug -u DieterReuter -b "$GIT_RELTEXT" $GIT_TAG builds/$BUILD_NR/
