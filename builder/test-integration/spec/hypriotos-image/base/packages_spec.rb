b90003d3df7e512d6352461e24fdbf78ab11c10c
Dieter Reuter <dieter.reuter@me.com>
2017-03-01 20:28:27 +0100

    Fix integrations tests for pi wifi and bluetooth firmware
    Signed-off-by: Dieter Reuter <dieter.reuter@me.com>

--- builder/test-integration/spec/hypriotos-image/base/packages_spec.rb
+++ builder/test-integration/spec/hypriotos-image/base/packages_spec.rb
@@ -22,26 +22,6 @@ describe package('usbutils') do
   it { should be_installed }
 end
 
-describe package('firmware-atheros') do
-  it { should be_installed }
-end
-
-describe package('firmware-brcm80211') do
-  it { should be_installed }
-end
-
-describe package('firmware-libertas') do
-  it { should be_installed }
-end
-
-describe package('firmware-ralink') do
-  it { should be_installed }
-end
-
-describe package('firmware-realtek') do
-  it { should be_installed }
-end
-
 describe package('lsb-release') do
   it { should be_installed }
 end
@@ -55,12 +35,7 @@ describe package('wireless-tools') do
 end
 
 describe 'for bluetooth-support' do
-  describe package('pi-bluetooth') do
-    it { should be_installed }
-  end
-
   describe package('bluetooth') do
     it { should be_installed }
   end
 end
-
