ea7f9197d0c6c00ac9a3002050b1bfa8e81da5d1
Dieter Reuter <dieter.reuter@me.com>
2017-03-01 20:43:37 +0100

    Fix integrations tests for Docker Machine
    Signed-off-by: Dieter Reuter <dieter.reuter@me.com>

--- builder/test-integration/spec/hypriotos-image/docker-machine_spec.rb
+++ builder/test-integration/spec/hypriotos-image/docker-machine_spec.rb
@@ -1,15 +1,5 @@
 require 'spec_helper'
 
-describe package('docker-machine') do
-  it { should be_installed }
-end
-
-describe command('dpkg -l docker-machine') do
-  its(:stdout) { should match /ii  docker-machine/ }
-  its(:stdout) { should match /0.8.2-36/ }
-  its(:exit_status) { should eq 0 }
-end
-
 describe file('/usr/local/bin/docker-machine') do
   it { should be_file }
   it { should be_mode 755 }
@@ -17,6 +7,6 @@ describe file('/usr/local/bin/docker-machine') do
 end
 
 describe command('docker-machine --version') do
-  its(:stdout) { should match /0.8.2/m }
+  its(:stdout) { should match /0.10.2/m }
   its(:exit_status) { should eq 0 }
 end
