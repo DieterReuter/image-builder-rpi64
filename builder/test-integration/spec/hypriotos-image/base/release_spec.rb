c7790aa639479accc836085581b83d359a2720b7
Dieter Reuter <dieter.reuter@me.com>
2018-01-07 16:38:20 +0100

    Update rootfs to v1.2.4
    - Using Debian Stretch directly
    - Remove all Stretch workarounds
    
    Signed-off-by: Dieter Reuter <dieter.reuter@me.com>

--- builder/test-integration/spec/hypriotos-image/base/release_spec.rb
+++ builder/test-integration/spec/hypriotos-image/base/release_spec.rb
@@ -7,7 +7,7 @@ describe file('/etc/os-release') do
   it { should be_owned_by 'root' }
   its(:content) { should contain /ID=debian/ }
   its(:content) { should match /HYPRIOT_OS="HypriotOS\/arm64"/ }
-  its(:content) { should match /HYPRIOT_OS_VERSION="v1.2.3"/ }
+  its(:content) { should match /HYPRIOT_OS_VERSION="v1.2.4"/ }
   its(:content) { should match /HYPRIOT_DEVICE="Raspberry Pi 3 64bit"/ }
   its(:content) { should match /HYPRIOT_IMAGE_VERSION=/ }
 end
@@ -17,7 +17,25 @@ describe file('/boot/os-release') do
   it { should be_owned_by 'root' }
   its(:content) { should contain /ID=debian/ }
   its(:content) { should match /HYPRIOT_OS="HypriotOS\/arm64"/ }
-  its(:content) { should match /HYPRIOT_OS_VERSION="v1.2.3"/ }
+  its(:content) { should match /HYPRIOT_OS_VERSION="v1.2.4"/ }
   its(:content) { should match /HYPRIOT_DEVICE="Raspberry Pi 3 64bit"/ }
   its(:content) { should match /HYPRIOT_IMAGE_VERSION=/ }
+end
+
+describe file('/etc/motd') do
+  it { should be_file }
+  it { should be_owned_by 'root' }
+  its(:content) { should match /HypriotOS \(Debian GNU\/Linux 9\)/ }
+end
+
+describe file('/etc/issue') do
+  it { should be_file }
+  it { should be_owned_by 'root' }
+  its(:content) { should match /HypriotOS \(Debian GNU\/Linux 9\)/ }
+end
+
+describe file('/etc/issue.net') do
+  it { should be_file }
+  it { should be_owned_by 'root' }
+  its(:content) { should match /HypriotOS \(Debian GNU\/Linux 9\)/ }
 end
