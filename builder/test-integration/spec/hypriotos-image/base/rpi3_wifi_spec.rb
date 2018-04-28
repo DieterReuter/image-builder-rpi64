eb5a38c85826003f99b1629bfc4b0f2b91e07c2d
Dieter Reuter <dieter.reuter@me.com>
2018-04-28 14:52:07 +0200

    Fix WiFi integration tests for 3B/3B+ boards
    Signed-off-by: Dieter Reuter <dieter.reuter@me.com>

--- builder/test-integration/spec/hypriotos-image/base/rpi3_wifi_spec.rb
+++ builder/test-integration/spec/hypriotos-image/base/rpi3_wifi_spec.rb
@@ -2,7 +2,7 @@
 cpu_info = command('cat /proc/cpuinfo').stdout
 
 if cpu_info.include?('a02082') or cpu_info.include?('a22082')
-  describe "RPi 3: built-in wifi works" do
+  describe "RPi 3B: built-in wifi works" do
     describe command('ifconfig -a') do
       its(:stdout) { should contain /wlan0/ }
     end
@@ -15,7 +15,7 @@ if cpu_info.include?('a02082') or cpu_info.include?('a22082')
   end
 end
 
-describe "RPi 3: built-in wifi firmware is installed" do
+describe "RPi 3B: built-in wifi firmware is installed" do
   describe file('/lib/firmware/brcm/brcmfmac43430-sdio.bin') do
     it { should be_file }
     it { should be_mode 644 }
@@ -29,14 +29,46 @@ describe "RPi 3: built-in wifi firmware is installed" do
   end
 end
 
-describe "RPi 3: built-in wifi works" do
+describe "RPi 3B: built-in wifi works" do
   describe command('ifconfig -a') do
     its(:stdout) { should contain /wlan0/ }
   end
 
-  describe command('ethtool -i wlan0') do
-    its(:stdout) { should contain /driver: brcmfmac/ }
-    its(:stdout) { should contain /version: 7.45.98.38/ }
-    its(:stdout) { should contain /firmware-version: 01-e58d219f/ }
+  wifi_info = command('ethtool -i wlan0').stdout
+  if wifi_info.include?('version: 7.45.98.38')
+    describe command('ethtool -i wlan0') do
+      its(:stdout) { should contain /driver: brcmfmac/ }
+      its(:stdout) { should contain /version: 7.45.98.38/ }
+      its(:stdout) { should contain /firmware-version: 01-e58d219f/ }
+    end
+  end
+end
+
+describe "RPi 3B+: built-in wifi firmware is installed" do
+  describe file('/lib/firmware/brcm/brcmfmac43455-sdio.bin') do
+    it { should be_file }
+    it { should be_mode 644 }
+    it { should be_owned_by 'root' }
+  end
+
+  describe file('/lib/firmware/brcm/brcmfmac43455-sdio.txt') do
+    it { should be_file }
+    it { should be_mode 644 }
+    it { should be_owned_by 'root' }
+  end
+end
+
+describe "RPi 3B+: built-in wifi works" do
+  describe command('ifconfig -a') do
+    its(:stdout) { should contain /wlan0/ }
+  end
+
+  wifi_info = command('ethtool -i wlan0').stdout
+  if wifi_info.include?('version: 7.45.154')
+    describe command('ethtool -i wlan0') do
+      its(:stdout) { should contain /driver: brcmfmac/ }
+      its(:stdout) { should contain /version: 7.45.154/ }
+      its(:stdout) { should contain /firmware-version: 01-4fbe0b04/ }
+    end
   end
 end
